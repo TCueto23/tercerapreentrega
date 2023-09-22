04d140d2ceb5a533867453cf6a84d11c771b829e
TCueto23 <tcueto2004@gmail.com>
2023-09-21 21:46:37 -0300

    primer commit

---
+++ products.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Buzo",
+        precio: 3000,
+        img: "https://drifters.com.ar/uploads/product_image/21478/650w_DriftersPDP_Thrasher_APP_TRSH340400-080_Shot11.jpg",
+    },
+    {
+        id: 2,
+        nombre: "Remera",
+        precio: 1500,
+        img: "https://drifters.com.ar/uploads/product_image/25309/650w_DriftersPDP_APP_TRSH510430-630_Shot1_v1.jpg",
+    },
+    {
+        id: 3,
+        nombre: "Jean",
+        precio: 5000,
+        img: "https://http2.mlstatic.com/D_NQ_NP_721962-MLA71669266542_092023-O.webp",
+    },
+    {
+        id: 4,
+        nombre: "Zapatillas",
+        precio: 6000,
+        img: "https://acdn.mitiendanube.com/stores/002/237/525/products/e0dbc41d-8936-450c-9b4c-5431f688e07e1-78add711fa2457a99b16683976302883-640-0.jpeg",
+    },
+];
